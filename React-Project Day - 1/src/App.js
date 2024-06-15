3e61217ed977f59f5cdd3b652d15345a2b8edf30
Diyapatel12 <diyapatel12032005@gmail.com>
2024-06-15 11:28:34 +0530

    update

--- React-Project Day - 1/src/App.js
+++ React-Project Day - 1/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 // import './App.css';
 
 function App() {
